1e1a1fa03c0592b6c14cd2d83df422df05dad841
Kallurisravani12 <KalluriSravani12>
2023-04-14 14:04:25 +0200

    My first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+console.log(10);
+console.log(10+20);
+console.log('Sravani');
+var a= 10;
+console.log(a);
+
+console.log(a+10);
+var b= 20,c=10;
+console.log(b+c);
+console.log(b-c);
+console.log(b*c);
+console.log(b/c);
+console.log(b%c);
+
+a= b+c
+b= b-c
+c= b*c
+d= b/c
+e= b%c
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+var a1=10,b1=5
+a1+=b1;
+console.log(a1);
+
+a1-=b1;
+console.log(a1);
+
+a1*=b1;
+console.log(a1);
+
+a1/=b1;
+console.log(a1);
+
+var c1=10
+c1++;
+console.log(c1);
+
+var s= 100, t= 200;
+console.log(s==t);
+console.log(s<t);
+console.log(s>t);
+console.log(s<=t);
+console.log(s>=t);
+
+
+console.log(s<=t && s>=t);
+console.log(s<=t || s>=t);
